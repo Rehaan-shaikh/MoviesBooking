2bb9fdead2977fc26a96dbc3bbfc4f4dbe1829fa
Rehaan-shaikh <rehanshaikh738500@gmail.com>
2025-10-01 23:24:12 +0530

    added comments

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { MenuIcon, SearchIcon, TicketPlus, XIcon } from "lucide-react";
 import { assets } from "../assets/assets";
-import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+// import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { user } = useUser();
-  const { openSignIn } = useClerk();
+  // const { user } = useUser();
+  // const { openSignIn } = useClerk();
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
       <Link to="/" className="max-md:flex-1">
@@ -18,9 +18,9 @@ const Navbar = () => {
 
       <div
         className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium
-max-md:text-lg z-50 flex flex-col md:flex-row items-center
-max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen
-min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${
+          max-md:text-lg z-50 flex flex-col md:flex-row items-center
+          max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen
+          min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${
           isOpen ? "max-md:w-full" : "max-md:w-0"
         }`}
       >
@@ -72,7 +72,7 @@ min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gr
 
       <div className="flex items-center gap-8">
         <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
-        {!user ? (
+        {/* {!user ? (
           <button
             onClick={openSignIn}
             className="px-4 py-1 sm:px-7 sm:py-2 bg-primary
@@ -87,11 +87,11 @@ cursor-pointer"
               <UserButton.Action
                 label="My Bookings"
                 labelIcon={<TicketPlus width={15} />}
-                onClick={()=>navigate('/my-bookings')}
+                onClick={() => navigate("/my-bookings")}
               />
             </UserButton.MenuItems>
           </UserButton>
-        )}
+        )} */}
       </div>
 
       <MenuIcon
--- client/src/components/Router.jsx
+++ client/src/components/Router.jsx
@@ -1,3 +1,5 @@
+// Modern v6.4+ Data Router (createBrowserRouter) setup
+// cause it Can handle loaders, actions, error boundaries, data fetching, etc and recommended for larger apps with nested routes
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import RootLayout from "../layout/RootLayout";
 import Home from "../pages/Home";
@@ -8,14 +10,19 @@ import Favorite from "../pages/Favorite";
 import MyBookings from "../pages/MyBookings";
 
 
+// Create router instance
 const router = createBrowserRouter(
   createRoutesFromElements(
+
+    // Root route ("/") uses RootLayout
+    // All child routes will render inside <Outlet /> of RootLayout
     <Route path="/" element={<RootLayout />}>
+
+      {/* Default (index) route → loads Home when path is exactly "/" */}
       <Route index element={<Home />} />
       <Route path="movies" element={<Movies />} />
       <Route path=":id" element={<MovieDetails />} />
       <Route path=":date" element={<SeatLayout />} />
-      <Route />
       <Route path="my-bookings" element={<MyBookings />} />
       <Route path="favorite" element={<Favorite />} />
     </Route>
