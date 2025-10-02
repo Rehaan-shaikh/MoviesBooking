fe78c2eae7e57aabb6dddcc48d9a0939237f8842
Rehaan-shaikh <rehanshaikh738500@gmail.com>
2025-10-02 13:39:39 +0530

    HomePageCommit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import FeaturedSection from '../components/FeaturedSection'
+import TrailersSection from '../components/TrailerComponent'
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
-      
+      <FeaturedSection />
+      <TrailersSection />
     </div>
   )
 }
